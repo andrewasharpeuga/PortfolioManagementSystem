89a5e9ae5b3fbba88eb84dba3d032cb99b897ec1
Andrew Sharpe <161970986+andrewasharpeuga@users.noreply.github.com>
2024-06-05 11:21:23 -0400

    Update Investment.java

--- Investment.java
+++ Investment.java
@@ -1,40 +1,21 @@
-package com.yourpackage;
+package com.portfolio.investment;
 
-import java.math.BigDecimal;
-
-public class Investment {
+public abstract class Investment {
     private String name;
-    private int quantity;
-    private BigDecimal purchasePrice;
-
-    public Investment(String name, int quantity, BigDecimal purchasePrice) {
+    private double amountInvested;
+    
+    public Investment(String name, double amountInvested) {
         this.name = name;
-        this.quantity = quantity;
-        this.purchasePrice = purchasePrice;
+        this.amountInvested = amountInvested;
     }
-
-    // Getters and setters
+    
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    
+    public double getAmountInvested() {
+        return amountInvested;
     }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public BigDecimal getPurchasePrice() {
-        return purchasePrice;
-    }
-
-    public void setPurchasePrice(BigDecimal purchasePrice) {
-        this.purchasePrice = purchasePrice;
-    }
+    
+    public abstract double getCurrentValue();
 }
